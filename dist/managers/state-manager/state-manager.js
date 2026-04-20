85e4ad8ec7cc714c6421727b006fec5bc447f0a4
Kevin McCarthy <kevin.e.mccarthy1983@gmail.com>
2026-04-20 15:23:32 +0100

    commit to commit

--- dist/managers/state-manager/state-manager.js
+++
@@ -1,9 +0,0 @@
-class StateManager {
-    isLightSpeed = false;
-    clickList = [];
-    activeClick = null;
-    orientation = null;
-    silencedAlien = false;
-    seenProjects = [];
-}
-export const state = new StateManager();
